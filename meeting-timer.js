0ec5746c556a1582fdbd373da50a2fdb725fa88c
Benjamin Ortiz <hello.ben.ortiz@gmail.com>
2015-09-13 00:27:08 -0700

    all basic functions complete, initial ui.

--- meeting-timer.js
+++ meeting-timer.js
@@ -1,11 +1,31 @@
-var nums = document.getElementById('nums'),
-    pause = document.getElementById('pause'),
-    start = document.getElementById('start'),
+var hrs = document.getElementById('hrs'),
+    mins = document.getElementById('mins'),
+    secs = document.getElementById('secs'),
+
+    hrsExist = document.getElementById('hrs-exist'),
+    minsExist = document.getElementById('mins-exist'),
+
+    hrsPlural = document.getElementById('hrs-plural'),
+    minsPlural = document.getElementById('mins-plural'),
+    secsPlural = document.getElementById('secs-plural'),
+    attendPlural = document.getElementById('attend-plural'),
+
+    control = document.getElementById('control'),
     reset = document.getElementById('reset'),
+    end = document.getElementById('end'),
+
+    mtPast = document.getElementById('mt-past-present'),
+    costPast = document.getElementById('cost-past-present'),
+
     attendInput = document.getElementById('attendees'),
+
+    cost = document.getElementById('cost'),
+
     seconds = 0, minutes = 0, hours = 0,
-    attendees = 1,
+    attendees = 2,
+    firstClick = true,
     t;
+  // FUTURE: remove global variables. use array for time store.
 
 function getInput() {
   // TODO return an error to the user
@@ -14,15 +34,20 @@ function getInput() {
     return attendees;
 
     // ensure the input is a number
-  } else if ( isNaN(Number(attendInput.value)) ) {
+  } else if (isNaN(Number(attendInput.value))) {
     return attendees;
 
     // no negative attendees
-  } else if ( Number(attendInput.value) <= 0) {
+  } else if (Number(attendInput.value) <= 0) {
     return attendees;
 
     // save the new value
+  } else if (Number(attendInput.value) === 1) {
+    attendPlural.textContent = "attendee";
+    attendees = attendInput.value;
+
   } else {
+    attendPlural.textContent = "attendees";
     attendees = attendInput.value;
   }
 }
@@ -33,17 +58,39 @@ function add() {
   var tempsec, tempmin;
 
   tempsec = seconds + Number(attendees);      // 80 = 40 + 40
-  seconds = tempsec%60;                       // 20 = 80 % 60
+  seconds = tempsec % 60;                       // 20 = 80 % 60
 
-  tempmin = minutes + Math.floor(tempsec/60); // 1  = 80 / 60
-  minutes = tempmin%60;                       // 1  = 1  % 60
+  tempmin = minutes + Math.floor(tempsec / 60); // 1  = 80 / 60
+  minutes = tempmin % 60;                       // 1  = 1  % 60
 
-  hours = hours + Math.floor(tempmin/60);     // 0  = 1  / 60
+  hours = hours + Math.floor(tempmin / 60);     // 0  = 1  / 60
 
-  nums.textContent =
-    (hours   ? (hours   > 9 ? hours   : "0" + hours)   : "00") + ":" +
-    (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") + ":" +
-    (seconds > 9 ? seconds : "0" + seconds);
+  hrs.textContent = hours;
+  mins.textContent = minutes;
+  secs.textContent = seconds;
+
+  if (hours >= 1) {
+    hrsExist.style.display = "inline";
+    hrsPlural.textContent = "hours";
+    if (hours === 1) {
+      hrsPlural.textContent = "hour";
+    }
+  }
+
+  if (minutes >= 1) {
+    minsExist.style.display = "inline";
+    minsPlural.textContent = "minutes";
+    if (minutes === 1) {
+      minsPlural.textContent = "minute";
+    }
+  }
+
+  if (seconds >= 1) {
+    secsPlural.textContent = "seconds";
+    if (seconds === 1) {
+      secsPlural.textContent = "second";
+    }
+  }
 
   timer();
 }
@@ -52,18 +99,52 @@ function timer() {
   t = setTimeout(add, 1000);
 }
 
-timer();
 
-// TODO handle double clicks by global status lock
 // Button functions
-start.onclick = timer;
-pause.onclick = function() {
+control.onclick = function () {
+  if (control.className === "start") {
+    control.className = "stop";
+    control.textContent = "Pause meeting.";
+    cost.style.display = "block";
+    end.style.display = "block";
+    timer();
+  } else if (control.className === "new") {
+    hrs.textContent = "0";
+    mins.textContent = "0";
+    secs.textContent = "0";
+    seconds = 0; minutes = 0; hours = 0;
+
+    cost.style.display = "none";
+
+    mtPast.textContent = "I am in a meeting with";
+    costPast.textContent = "So far, this meeting has cost";
+
+    attendInput.value = 2;
+
+    control.textContent = "Start meeting.";
+    control.className = "start";
+
+  } else {
+    control.className = "start";
+    control.textContent = "Continue meeting."
+    clearTimeout(t);
+  }
+}
+
+end.onclick = function () {
   clearTimeout(t);
+  mtPast.textContent = "I was in a meeting with";
+  costPast.textContent = "This meeting cost";
+  end.style.display = "none";
+  control.className = "new";
+  control.textContent = "New meeting.";
 }
-reset.onclick = function() {
-  nums.textContent = "00:00:00";
-  seconds = 0; minutes = 0; hours = 0;
-}
-attendInput.onkeyup = function() {
+
+
+attendInput.onkeyup = function () {
   getInput();
+}
+
+attendInput.onclick = function () {
+  this.select();
 }
